b0e6a3814afd637cc7e6012ec2d18476740fd624
Faezeh <faezehadabi9@gmail.com>
2025-11-08 10:01:24 +0330

    Modify the code

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,11 +25,10 @@ module.exports = {
         119: '476px'
       },
       gridTemplateColumns: {
-        "moarefi": 'repeat(auto-fit , minmax(120px , 1fr))'
+        "moarefi": 'repeat(auto-fit , minmax(120px , 1fr))',
+                "shegeftangiz": 'repeat(auto-fit , minmax(270px , 1fr))',
       },
-      gridTemplateColumns: {
-        "shegeftangiz": 'repeat(auto-fit , minmax(270px , 1fr))'
-      },
+  
       fontSize: {
         'fluid-base': 'clamp(1rem, 2vw, 1.5rem)',     // برای متن معمولی
         'fluid-lg': 'clamp(1.5rem, 4vw, 2.5rem)',      // برای عنوان‌ها
