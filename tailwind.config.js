82d6e7cdb02d0ee8f7d303252f90ae44de3a943f
Faezeh <faezehadabi9@gmail.com>
2025-11-08 21:38:00 +0330

    setting shekftangiz section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}",
+    content: ["./public/**/*.{html,js}", "./node-modules/flowbit/**/*.js"],
+    theme: {
+        screens: {
+            sm: "570px",
+            md: "768px",
+            lg: "992px",
+            xl: "1200px",
+        },
+        container: {
+            center: true,
+        },
+        extend: {
+            fontFamily: {
+                roboto: "Open Sans, sans-serif",
+            },
+            flex: {
+                2: "2 2 0",
+            },
+            spacing: {
+                111: "444px",
+                119: "476px",
+            },
+            gridTemplateColumns: {
+                moarefi: "repeat(auto-fit , minmax(120px , 1fr))",
+                shegeftangiz: "repeat(auto-fit , minmax(200px , 1fr))",
+            },
 
-    "./node-modules/flowbit/**/*.js"],
-  theme: {
-    screens: {
-      sm: '570px',
-      md: '768px',
-      lg: '992px',
-      xl: '1200px',
+            fontSize: {
+                "fluid-base": "clamp(1rem, 2vw, 1.5rem)", // برای متن معمولی
+                "fluid-lg": "clamp(1.5rem, 4vw, 2.5rem)", // برای عنوان‌ها
+                "fluid-lg1": "clamp(1.3rem, 4vw, 2.2rem)", // برای عنوان‌ها
+                "fluid-sm": "clamp(0.875rem, 1.5vw, 1rem)", // برای متن کوچک
+            },
+        },
     },
-    container: {
-      center: true,
-    },
-    extend: {
-      fontFamily: {
-        "roboto": 'Open Sans, sans-serif',
-      },
-      flex: {
-        2: '2 2 0'
-      },
-      spacing: {
-        111: '444px',
-        119: '476px'
-      },
-      gridTemplateColumns: {
-        "moarefi": 'repeat(auto-fit , minmax(120px , 1fr))',
-                "shegeftangiz": 'repeat(auto-fit , minmax(270px , 1fr))',
-      },
-  
-      fontSize: {
-        'fluid-base': 'clamp(1rem, 2vw, 1.5rem)',     // برای متن معمولی
-        'fluid-lg': 'clamp(1.5rem, 4vw, 2.5rem)',      // برای عنوان‌ها
-        'fluid-sm': 'clamp(0.875rem, 1.5vw, 1rem)',    // برای متن کوچک
-      },
-    },
-  },
-  plugins: [
-    require('flowbite/plugin'),
-  ],
+    plugins: [require("flowbite/plugin")],
+};
+plugins: {
+
+    [require("flowbite/plugin")]
 }
-
