0fad3fa574b9c83d9e4dd11883efb950a83e214b
Faezeh <faezehadabi9@gmail.com>
2025-11-09 22:02:19 +0330

    modify shekeftangiz

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
             md: "768px",
             lg: "992px",
             xl: "1200px",
+            xxl: "1400px"
         },
         container: {
             center: true,
@@ -21,23 +22,22 @@ module.exports = {
             spacing: {
                 111: "444px",
                 119: "476px",
+                135: "540"
+
             },
             gridTemplateColumns: {
                 moarefi: "repeat(auto-fit , minmax(120px , 1fr))",
-                shegeftangiz: "repeat(auto-fit , minmax(200px , 1fr))",
+                shegeftangiz: "repeat(auto-fit , minmax(130px , 1fr))",
             },
 
             fontSize: {
                 "fluid-base": "clamp(1rem, 2vw, 1.5rem)", // برای متن معمولی
                 "fluid-lg": "clamp(1.5rem, 4vw, 2.5rem)", // برای عنوان‌ها
                 "fluid-lg1": "clamp(1.3rem, 4vw, 2.2rem)", // برای عنوان‌ها
+                "fluid-lg2": "clamp(1.1rem, 4vw, 2rem)", // برای عنوان‌ها
                 "fluid-sm": "clamp(0.875rem, 1.5vw, 1rem)", // برای متن کوچک
             },
         },
     },
     plugins: [require("flowbite/plugin")],
 };
-plugins: {
-
-    [require("flowbite/plugin")]
-}
